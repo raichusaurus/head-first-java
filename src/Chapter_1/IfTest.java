824af534bce2f11cb0e144ce6a4743c87914a4f8
felixerus <felixerus@gmail.com>
2018-01-22 15:26:11 -0800

    Add else statement to if condition in if test class

--- src/Chapter_1/IfTest.java
+++ src/Chapter_1/IfTest.java
@@ -3,9 +3,11 @@ package Chapter_1;
 public class IfTest {
 
     public static void main (String[] args) {
-        int x = 3;
+        int x = 2;
         if (x == 3) {
             System.out.println("x must be 3");
+        } else {
+            System.out.println("x is NOT 3");
         }
         System.out.println("This runs no matter what");
     }
