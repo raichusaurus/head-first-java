32f19da5e9371614359eed46df4a954172841e15
felixerus <felixerus@gmail.com>
2018-01-24 16:07:27 -0800

    Create GameLauncher class to do more than just operate in main()

---
+++ src/Chapter_2/TapeDeck/TapeDeck.java
@@ -0,0 +1,14 @@
+package Chapter_2.TapeDeck;
+
+public class TapeDeck {
+
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println("tape playing");
+    }
+
+    void recordTape() {
+        System.out.println("tape recording");
+    }
+}
