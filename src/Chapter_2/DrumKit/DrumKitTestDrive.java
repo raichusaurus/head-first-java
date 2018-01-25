bae71668e7aab76ecf6d5f3dc6432476a0d72eb4
felixerus <felixerus@gmail.com>
2018-01-24 16:22:14 -0800

    Create DrumKit and test class

---
+++ src/Chapter_2/DrumKit/DrumKitTestDrive.java
@@ -0,0 +1,16 @@
+package Chapter_2.DrumKit;
+
+public class DrumKitTestDrive {
+
+    public static void main(String[] args) {
+
+        DrumKit d = new DrumKit();
+
+        d.playSnare();
+        d.snare = false;
+        if (d.snare == true) {
+            d.playSnare();
+        }
+        d.playTopHat();
+    }
+}
