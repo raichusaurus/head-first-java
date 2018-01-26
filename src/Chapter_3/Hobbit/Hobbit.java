ec2ad9d2463fde21ba9da424e4beff540d0f4c54
felixerus <felixerus@gmail.com>
2018-01-26 15:43:43 -0800

    Create example (ugly)Hobbit class

---
+++ src/Chapter_3/Hobbit/Hobbit.java
@@ -0,0 +1,27 @@
+package Chapter_3.Hobbit;
+
+public class Hobbit {
+
+    String name;
+
+    public static void main (String[] args) {
+
+        Hobbit[] h = new Hobbit[3];
+        int z = 0;
+
+        while (z < 2) {
+            z = z + 1;
+            h[z] = new Hobbit();
+            h[z].name = "bilbo";
+            if (z == 1) {
+                h[z].name = "frodo";
+            }
+            if (z == 2) {
+                h[z].name = "sam";
+            }
+
+            System.out.print(h[z].name + " is a ");
+            System.out.println("good Hobbit name");
+        }
+    }
+}
