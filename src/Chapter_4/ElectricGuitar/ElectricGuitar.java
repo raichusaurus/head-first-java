1ecb8009f21fd8f8cc695c24daab14e9f266e6f5
felixerus <felixerus@gmail.com>
2018-01-26 17:01:05 -0800

    Create ElectricGuitar class

---
+++ src/Chapter_4/ElectricGuitar/ElectricGuitar.java
@@ -0,0 +1,32 @@
+package Chapter_4.ElectricGuitar;
+
+public class ElectricGuitar {
+
+    private String brand;
+    private int numOfPickups;
+    private boolean rockStarUsesIt;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getNumOfPickups() {
+        return numOfPickups;
+    }
+
+    public void setNumOfPickups(int numOfPickups) {
+        this.numOfPickups = numOfPickups;
+    }
+
+    public boolean isRockStarUsesIt() {
+        return rockStarUsesIt;
+    }
+
+    public void setRockStarUsesIt(boolean rockStarUsesIt) {
+        this.rockStarUsesIt = rockStarUsesIt;
+    }
+}
