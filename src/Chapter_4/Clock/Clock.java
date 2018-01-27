2672162d671972014a624185a253c8b08c5ac48b
felixerus <felixerus@gmail.com>
2018-01-26 17:18:54 -0800

    Create Clock and Clock test class.

---
+++ src/Chapter_4/Clock/Clock.java
@@ -0,0 +1,14 @@
+package Chapter_4.Clock;
+
+public class Clock {
+
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
