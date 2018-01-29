0c9bd32c4e90b84af2865631feef7fd6f833d81a
felixerus <felixerus@gmail.com>
2018-01-29 12:58:19 -0800

    Implement checkYourself method.

--- src/Chapter_5/SimpleDotCom.java
+++ src/Chapter_5/SimpleDotCom.java
@@ -2,14 +2,34 @@ package Chapter_5;
 
 public class SimpleDotCom {
 
+    int numOfHits;
+    private int[] locationCells;
+
     // stub
     public void setLocationCells(int[] cellLocations) {
 
     }
 
+    public String checkYourself(String stringGuess) {
 
-    // stub
-    public String checkYourself(String guess) {
-        return guess;
+        int guess = Integer.parseInt(stringGuess);
+
+        String result = "miss";
+
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+
+        System.out.println(result);
+
+        return result;
     }
 }
