3ef3ccb6caa4ded5c61f5f0d00098076cff01850
felixerus <felixerus@gmail.com>
2018-01-29 12:51:50 -0800

    Create SimpleDotCom stub class and test class prior to implementing Sink a DotCom game(aka battleship).

---
+++ src/Chapter_5/SimpleDotCom.java
@@ -0,0 +1,15 @@
+package Chapter_5;
+
+public class SimpleDotCom {
+
+    // stub
+    public void setLocationCells(int[] cellLocations) {
+
+    }
+
+
+    // stub
+    public String checkYourself(String guess) {
+        return guess;
+    }
+}
